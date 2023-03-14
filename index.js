5eeeecd24ba7c068a6c38a4a3c4569c4bef59d17
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-14 11:31:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Declare a variable in global scope called customerName using the var keyword.
+var customerName = "bob";
+
+// Write a function that accesses that global customerName variable, and uppercases it.
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Write a function that when called, declares a variable called bestCustomer 
+// in global scope and assigns it to be 'not bob'.
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// Write a new function called overwriteBestCustomer() that changes that bestCustomer variable.
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Now declare a constant (using the const keyword) in global scope called leastFavoriteCustomer; 
+// be sure to assign it some initial value.
+const leastFavoriteCustomer = "Karen";
+
+// Finally, write a function called changeLeastFavoriteCustomer() that attempts 
+// to change that constant - notice what JavaScript does when you try to change the constant.
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Tom"; // This will result in a TypeError as we cannot change a const variable
+}
